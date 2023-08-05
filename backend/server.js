e2aec2fedbe3e6a0ea3eb8296aaaed3f838d0f2b
Harkirat <hs3188248@gmail.com>
2023-08-05 17:11:22 +0530

    added backend

---
+++ backend/server.js
@@ -0,0 +1,72 @@
+const express = require('express')
+
+const app = express();
+
+const cors = require('cors')
+app.use(cors({origin:"*"}))
+
+app.use(express.json())
+
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = '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';
+
+app.post('/send-message',(req,res)=>{
+
+    const { name,email,subject,query}= req.body;
+    console.log(name,email,subject,query)
+    if(!name || !email || !query ){
+        res.send(500)
+    }
+    else{
+        new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail({
+
+            "sender":{ "email":"admin@techzmechanics.com", "name":"Techz Mechanics"},
+            "subject":`${name} has a query posted on your website`,
+            "htmlContent":`<!DOCTYPE html>
+            <html>
+            <body>
+            <h2>Hi Admin , Following are the details</h2>
+            <p><b>Name:</b> ${name}</p>
+            <p><b>Email:</b> ${email}</p>
+            <p><b>Subject:</b> ${subject}</p>
+            <p><b>Query:</b> ${query}</p>
+            <br>
+            <p>Thanks & Regards</p>
+            </body>
+            </html>`,
+            "params":{
+                "greeting":"Hi Admin",
+                "headline":`${name} has a query posted on your website`
+             },
+            
+            "to":[{
+                "email":"techz.mechanics@gmail.com"
+               
+            }]
+      
+       
+       }).then(function(data) {
+         console.log(data);
+         res.json({"message":"sucess"})
+       }, function(error) {
+         console.error(error);
+       });
+    }
+   
+
+
+
+
+
+
+
+
+
+    
+})
+
+
+
+app.listen(8080,()=>{
+    console.log("Server is running at http://localhost:8080")
+})
